27c8b290e8a42da8ce1dc2d1dc1fe97b8fcf45e9
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-03-18 21:40:02 +0800

    Initial commit

---
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kotlin.mall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
