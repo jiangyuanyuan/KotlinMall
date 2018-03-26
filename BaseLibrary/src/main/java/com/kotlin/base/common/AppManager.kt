142b80ad8adaeae476f8a179c1d508c523c0261d
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-03-26 10:38:40 +0800

    0326  1038 AppManager 封装

---
+++ BaseLibrary/src/main/java/com/kotlin/base/common/AppManager.kt
@@ -0,0 +1,60 @@
+package com.kotlin.base.common
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import org.jetbrains.anko.activityManager
+import java.util.*
+
+/**
+ * Created by jiangyuanyuan on 26/3/18.
+ */
+class AppManager private constructor(){
+    private val activityStack:Stack<Activity> = Stack()
+
+    companion object {
+        val instance:AppManager by lazy { AppManager() }
+    }
+    /*
+        Activity入栈
+     */
+    fun addActivity(activity:Activity){
+        activityStack.add(activity)
+    }
+    /*
+        Activity出栈
+     */
+    fun removeActivity(activity:Activity){
+        activity.finish()
+        activityStack.remove(activity)
+    }
+    /*
+        获取当前栈顶
+     */
+    fun currentActivity():Activity{
+        return activityStack.lastElement()
+    }
+    /*
+        清理栈
+     */
+
+    fun finishAllActivitys(){
+        for (activity in activityStack){
+            activity.finish()
+        }
+        activityStack.clear()
+    }
+
+    /*
+        退出应用程序
+     */
+    @SuppressLint("MissingPermission")
+    fun exitApp(context:Context){
+        finishAllActivitys()
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.killBackgroundProcesses(context.packageName)
+        System.exit(0)
+    }
+
+}
--- BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
@@ -1,10 +1,21 @@
 package com.kotlin.base.ui.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kotlin.base.common.AppManager
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 
 /**
  * Created by jiangyuanyuan on 18/3/18.
  */
 open class BaseActivity : RxAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppManager.instance.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instance.removeActivity(this)
+    }
 }
