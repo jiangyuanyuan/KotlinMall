bff6973a0cbf0a0fbba8c9a5105fe2c930e7e65b
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-03-26 10:12:45 +0800

    0326  1012  lamda扩展

--- BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
@@ -1,5 +1,8 @@
 package com.kotlin.base.ext
 
+import android.view.View
+import com.kotlin.base.data.protocol.BaseResp
+import com.kotlin.base.rx.BaseFunBoolean
 import com.kotlin.base.rx.BaseSubscriber
 import com.trello.rxlifecycle.LifecycleProvider
 import rx.Observable
@@ -14,4 +17,8 @@ fun <T> Observable<T>.execute(subscriber: BaseSubscriber<T>,activity: LifecycleP
             .compose(activity.bindToLifecycle())
             .subscribeOn(Schedulers.io())
             .subscribe(subscriber)
+}
+
+fun View.onClick(method:() ->Unit){
+    this.setOnClickListener{method}
 }
