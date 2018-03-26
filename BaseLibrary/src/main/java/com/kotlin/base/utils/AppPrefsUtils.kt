1158110b3f811715172dd14e51cdc3caf54f9d6f
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-03-26 17:43:33 +0800

    0326  1730 通用样式导入

--- BaseLibrary/src/main/java/com/kotlin/base/utils/AppPrefsUtils.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/utils/AppPrefsUtils.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import com.kotlin.base.BaseApplication
-import com.kotlin.base.common.BaseApplication
 import com.kotlin.base.common.BaseConstant
 
 /*
