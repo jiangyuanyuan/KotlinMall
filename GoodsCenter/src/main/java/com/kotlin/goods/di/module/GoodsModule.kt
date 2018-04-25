bbf1281be86ca0a043842abdf606ea99d0c59e6f
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-04-25 20:31:34 +0800

    dagger2修复

--- GoodsCenter/src/main/java/com/kotlin/goods/di/module/GoodsModule.kt
+++ GoodsCenter/src/main/java/com/kotlin/goods/di/module/GoodsModule.kt
@@ -1,13 +1,14 @@
 package com.kotlin.goods.di.module
 
 import com.kotlin.goods.service.GoodsService
+import com.kotlin.goods.service.impl.GoodsServiceImpl
 import dagger.Module
 import dagger.Provides
 
 @Module
 class GoodsModule {
     @Provides
-    fun providersGoodsService(service:GoodsService):GoodsService{
+    fun providersGoodsService(service: GoodsServiceImpl):GoodsService{
         return service
     }
 }
